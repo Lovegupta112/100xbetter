274b3fe3ad7c1bcb993a2415280d5f21c6af4e7e
lovegupta112 <loveguptaofficials@gmail.com>
2024-09-23 01:24:48 +0530

    updated middleware for  user

--- web2/week8/course-selling-app/middlewares/authMiddleware.js
+++ web2/week8/course-selling-app/middlewares/authMiddleware.js
@@ -1,44 +1,39 @@
-const jwt=require('jsonwebtoken');
-require('dotenv').config();
-const jwtAdminSecret=process.env.JWT_ADMIN_SECRET;
-const jwtUserSecret=process.env.JWT_USER_SECRET;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const jwtAdminSecret = process.env.JWT_ADMIN_SECRET;
+const jwtUserSecret = process.env.JWT_USER_SECRET;
 
+const authUserMiddleware = (req, res, next) => {
+  try {
+    const authorization = req.headers.authorization;
 
-const authUserMiddleware=(req,res,next)=>{
- try{
-  const authorization=req.headers.authorization;
+    const verified = jwt.verify(authorization, jwtUserSecret);
 
-  const verified= jwt.verify(authorization,jwtUserSecret);
-
-  console.log('verified 13..',verified);
-   if(verified ){
-        next();
-   }
-   else{
+    if (verified) {
+      req.userId=verified.userId;
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (error) {
     res.sendStatus(401);
-   }
- }
- catch(error){
-    res.sendStatus(401);
- }
-}
-const authAdminMiddleware=(req,res,next)=>{
- try{
-  const authorization=req.headers.authorization;
+  }
+};
+const authAdminMiddleware = (req, res, next) => {
+  try {
+    const authorization = req.headers.authorization;
 
-  const verified= jwt.verify(authorization,jwtAdminSecret);
+    const verified = jwt.verify(authorization, jwtAdminSecret);
 
-   if(verified){ 
-      req.adminId=verified.userId
-        next();
-   }
-   else{
+    if (verified) {
+      req.adminId = verified.userId;
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (error) {
     res.sendStatus(401);
-   }
- }
- catch(error){
-    res.sendStatus(401);
- }
-}
+  }
+};
 
-module.exports={authUserMiddleware,authAdminMiddleware};
+module.exports = { authUserMiddleware, authAdminMiddleware };
