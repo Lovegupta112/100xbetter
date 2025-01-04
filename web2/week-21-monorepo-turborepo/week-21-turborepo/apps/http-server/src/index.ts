3165ff1377f5ca80341194102430a580194924a1
lovegupta112 <loveguptaofficials@gmail.com>
2025-01-05 01:06:28 +0530

    updated turbo.json

--- web2/week-21-monorepo-turborepo/week-21-turborepo/apps/http-server/src/index.ts
+++ web2/week-21-monorepo-turborepo/week-21-turborepo/apps/http-server/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 const app=express();
-const PORT=3000;
+const PORT=3001;
 
 app.get('/health',(req,res)=>{
     res.send('server is healthy.');
 })
 
+app.get('signup',(req,res)=>{
+    
+})
+app.get('signin',(req,res)=>{
+
+})
+app.get('/room-chat',(req,res)=>{
+
+})
 
 app.listen(PORT,()=>console.log(`Server is listening on port : ${PORT}`));
