2a0735706b3f54b2c4cfc0875efad1c57f6bf3f8
lovegupta112 <loveguptaofficials@gmail.com>
2025-01-22 22:43:46 +0530

    updated auth middleware

--- web2/week-22-project/draw-app/apps/http-backend/src/middleware/authMiddleware.ts
+++ web2/week-22-project/draw-app/apps/http-backend/src/middleware/authMiddleware.ts
@@ -1,6 +1,28 @@
-import { Request,Response,NextFunction } from "express";
-const authMiddleware=(req:Request,res:Response,next:NextFunction)=>{
+import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET_KEY } from "@repo/backend-common/config";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
+interface AuthRequest extends Request {
+  userEmail: string;
 }
+const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers.authorization?.split("")[1];
+
+    if (!token) {
+      res.status(401).send("Token not present.");
+      return;
+    }
+    const decoded = jwt.verify(token, JWT_SECRET_KEY) as JwtPayload;
+    req.userEmail = decoded.email;
+    next();
+  } catch (error) {
+    res.status(401).json({message:'Invalid Token!'});
+  }
+};
 
 export default authMiddleware;
